7ef7a9d9d9cb621102c10cd4101d63f883530c7a
m-bt <michalbiernat@windowslive.com>
2020-02-21 14:43:58 +0100

    Update #1
    Added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float range = 5.0f;
+
+    private float clampMin, clampMax;
+
+    private Rigidbody rb;
+
+    private bool isReady = false;
+
+    private void Start()
+    {
+        float z = transform.position.z;
+        clampMin = z - range;
+        clampMax = z + range;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(float input)
+    {
+        Vector3 position = transform.position;
+        position.z += input;
+
+        position.z = Mathf.Clamp(position.z, clampMin, clampMax);
+
+        transform.position = position;
+    }
+
+    public void Splash(float force)
+    {
+        if (isReady)
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isReady = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isReady = true;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Player player1, player2;
+
+    private float inputPlayer1, inputPlayer2;
+
+    public float speed, force;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        inputPlayer1 = Input.GetAxis("Player1 Movement");
+        inputPlayer2 = Input.GetAxis("Player2 Movement");
+
+        if (Input.GetButtonDown("Player1 Splash"))
+            player1.Splash(force);
+
+        if (Input.GetButtonDown("Player2 Splash"))
+            player2.Splash(force);
+    }
+
+    private void FixedUpdate()
+    {
+        player1.Move(inputPlayer1 * speed * Time.fixedDeltaTime);
+        player2.Move(inputPlayer2 * speed * Time.fixedDeltaTime);
+    }
+}
