266a7d2443e27e331b61d9a444e5e5c0aa086ca7
m-bt <michalbiernat@windowslive.com>
2020-02-23 17:59:22 +0100

    Update #7

---
+++ Assets/Scripts/Continue.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Continue : MonoBehaviour
+{
+    public enum Mode
+    {
+        None,
+        Continue,
+        Play,
+        Replay
+    };
+
+    public Mode currentMode;
+
+    public Animator animator;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Execute(currentMode);
+        }
+    }
+
+    private void Execute(Mode mode)
+    {
+        switch (mode)
+        {
+            case Mode.None:
+                break;
+
+            case Mode.Continue:
+                animator.SetTrigger("continue");
+                break;
+
+            case Mode.Play:
+                break;
+
+            case Mode.Replay:
+                break;
+        }
+    }
+}
