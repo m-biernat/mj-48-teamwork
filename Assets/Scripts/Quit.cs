abe252c01f4c568e85afd2cb23712fc9720bf970
m-bt <michalbiernat@windowslive.com>
2020-02-24 17:40:55 +0100

    Win64 build support

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    private void Start()
+    {
+#if UNITY_STANDALONE
+        DontDestroyOnLoad(gameObject);
+#else
+        enabled = false;
+#endif
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
